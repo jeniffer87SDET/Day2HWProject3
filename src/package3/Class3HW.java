152411e5f6cb140628e96ab4418841dde1391d8b
Rimsky <Rimsky@DESKTOP-BO2IQFC.lan>
2023-04-09 21:38:28 -0400

    "Homework 3 - DONE!"

---
+++ src/package3/Class3HW.java
@@ -0,0 +1,13 @@
+package package3;
+
+public class Class3HW {
+
+	public static void main (String args []) {
+		
+		System.out.println("Let's pratice even more!");
+	}
+	
+	public void divide (int a, int b) {
+		System.out.println(a/b);
+	}
+}
